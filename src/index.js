623c93d80a5f5ce5505f96c51f9ee3146f63916a
Rui-Araujo1409 <rui1409@gmail.com>
2025-11-03 16:41:38 +0000

    config botões para mudar conteúdos

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
 import "./styles.css";
 import {títuloElemento, imagem, textoElemento} from "./homepage.js";
+import {textoDiv} from "./sobre.js";
+
+const conteúdo = document.querySelector("#conteudo");
+
+const botãoHomepage = document.querySelector("#home");
+const botãoMenu = document.querySelector("#menu");
+const botãoLocal = document.querySelector("#local")
+const botãoSobre = document.querySelector("#sobre");
+
+botãoSobre.addEventListener("click", () => {
+conteúdo.innerHTML = "";
+conteúdo.innerHTML = "Sobre";
+});
+
+botãoHomepage.addEventListener("click", () => {
+    conteúdo.innerHTML = "";
+    conteúdo.textContent = "Home";
+});
+
+botãoMenu.addEventListener("click", () =>{
+    conteúdo.innerHTML = "";
+    conteúdo.textContent = "Menu";
+});
+
+botãoLocal.addEventListener("click", () => {
+    conteúdo.innerHTML = "";
+    conteúdo.textContent = "Local";
+});
---
+++ src/sobre.js
@@ -0,0 +1,11 @@
+const texto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+const textoDiv = document.createElement("div");
+const textoSobre = document.createElement("p");
+
+textoDiv.appendChild(textoSobre);
+textoDiv.setAttribute("id", "sobre");
+textoSobre.setAttribute("id", "textoSobre");
+textoSobre.textContent = texto;
+
+export {textoDiv};
